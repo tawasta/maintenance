caa1f6629e58387dff687e80fe22a5876fd59671
difbit <timo.kekalainen@tawasta.fi>
2024-11-25 14:42:26 +0200

    [ADD] A new module: maintenance_equipment_form_view_field_changes. It adds two fields and modifies equipment form view

---
+++ maintenance_equipment_form_view_field_changes/__manifest__.py
@@ -0,0 +1,37 @@
+##############################################################################
+#
+#    Author: Oy Tawasta OS Technologies Ltd.
+#    Copyright 2024 Oy Tawasta OS Technologies Ltd. (https://tawasta.fi)
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program. If not, see http://www.gnu.org/licenses/agpl.html
+#
+##############################################################################
+
+{
+    "name": "Maintenance Equipment - Form view field changes",
+    "summary": "Modified field arrangements and names on Maintenance Equipment form view",
+    "version": "17.0.1.0.0",
+    "category": "Maintenance",
+    "website": "https://github.com/tawasta/odoo/maintenance",
+    "author": "Tawasta",
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "depends": [
+        "maintenance",
+    ],
+    "data": [
+        "views/maintenance_views.xml",
+    ],
+}
